e12c8650dde0dfe5eaec7a194a2df9f73e3bb54b
flabbet <flubbet@gmail.com>
2024-11-07 09:12:48 +0100

    Removed HTML5 canvas backend

--- src/Drawie.Backend.Html5Canvas/HtmlObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-namespace Drawie.Html5Canvas;
-
-public class HtmlObject
-{
-    private static int HandleCounter = 0;
-    public int Handle { get; } = HandleCounter++;
-}
--- src/Drawie.Backend.Html5Canvas/Impl/HtmlObjectImpl.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Concurrent;
-
-namespace Drawie.Html5Canvas.Impl;
-
-public class HtmlObjectImpl<T>
-{
-    public ConcurrentDictionary<int, T> ManagedObjects { get; } = new ConcurrentDictionary<int, T>();
-}
