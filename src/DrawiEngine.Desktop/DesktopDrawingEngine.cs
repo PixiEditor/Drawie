46c5b14c55469f1a8c5917810078ed34bdb4fed5
Krzysztof Krysiński <flubbet@gmail.com>
2024-12-22 15:34:13 +0100

    default desktop opengl for macos

--- src/DrawiEngine.Desktop/DesktopDrawingEngine.cs
+++ src/DrawiEngine.Desktop/DesktopDrawingEngine.cs
@@ -1,4 +1,5 @@
-﻿using Drawie.RenderApi.OpenGL;
+﻿using Drawie.RenderApi;
+using Drawie.RenderApi.OpenGL;
 using Drawie.RenderApi.Vulkan;
 using Drawie.Silk;
 using Drawie.Skia;
@@ -9,7 +10,14 @@ public static class DesktopDrawingEngine
 {
     public static DrawingEngine CreateDefaultDesktop()
     {
-        VulkanRenderApi renderApi = new VulkanRenderApi();
-        return new DrawingEngine(renderApi, new GlfwWindowingPlatform(renderApi), new SkiaDrawingBackend(), new DrawieRenderingDispatcher());
+        IRenderApi renderApi = new VulkanRenderApi();
+
+        if (OperatingSystem.IsMacOS())
+        {
+            renderApi = new OpenGlRenderApi();
+        }
+
+        return new DrawingEngine(renderApi, new GlfwWindowingPlatform(renderApi), new SkiaDrawingBackend(),
+            new DrawieRenderingDispatcher());
     }
 }
