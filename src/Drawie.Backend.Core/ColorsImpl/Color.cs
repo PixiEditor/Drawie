90d6ea44b3829eeb6bc945a0e5e59935d3ce2daa
Krzysztof Krysiński <flubbet@gmail.com>
2025-03-10 12:50:29 +0100

    Improved ToHex methods

--- src/Drawie.Backend.Core/ColorsImpl/Color.cs
+++ src/Drawie.Backend.Core/ColorsImpl/Color.cs
@@ -237,10 +237,15 @@ namespace Drawie.Backend.Core.ColorsImpl
         /// <summary>
         ///     Returns hex string representation of the color.
         /// </summary>
-        /// <returns>Color string in format: AARRGGBB</returns>
-        public string? ToHex()
+        /// <returns>Color string in format: RRGGBB</returns>
+        public string? ToRgbHex()
         {
-            return this == Empty ? null : $"{this._colorValue:X8}";
+            return this == Empty ? null : $"#{this.R:x2}{this.G:x2}{this.B:x2}";
+        }
+
+        public string? ToArgbHex()
+        {
+            return this == Empty ? null : $"#{this._colorValue:X8}";
         }
 
         public static Color Lerp(Color from, Color to, double t)
