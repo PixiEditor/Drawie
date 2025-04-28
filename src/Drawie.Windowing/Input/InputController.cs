0046c7d83f551e211b678271dcb266df9918f19d
Krzysztof Krysiński <flubbet@gmail.com>
2025-04-28 23:41:35 +0200

    Added default keyboard

--- src/Drawie.Windowing/Input/InputController.cs
+++ src/Drawie.Windowing/Input/InputController.cs
@@ -2,6 +2,7 @@
 
 public class InputController
 {
+    public IKeyboard? DefaultKeyboard => Keyboards.FirstOrDefault();
     public IReadOnlyCollection<IKeyboard> Keyboards { get; }
     
     public InputController(IEnumerable<IKeyboard> keyboards)
