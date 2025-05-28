6a22e1b5affd0906a949f97b755ae588eda7980a
Krzysztof Krysiński <flubbet@gmail.com>
2025-05-28 10:40:17 +0200

    Fixed encoding bmp

--- src/Drawie.Backend.Skia/Encoders/BmpEncoder.cs
+++ src/Drawie.Backend.Skia/Encoders/BmpEncoder.cs
@@ -9,20 +9,17 @@ public class BmpEncoder : IImageEncoder
 {
     public byte[] Encode(Image image)
     {
-        Image toEncode = image;
-        if (image.Info.BytesPerPixel is not (3 or 4))
-        {
-            using DrawingSurface surface = DrawingSurface.Create(new ImageInfo(
-                image.Info.Width,
-                image.Info.Height,
-                ToValidColorType(image.Info.BytesPerPixel),
-                AlphaType.Premul,
-                image.Info.ColorSpace)
-            );
+        Image toEncode;
+        using DrawingSurface surface = DrawingSurface.Create(new ImageInfo(
+            image.Info.Width,
+            image.Info.Height,
+            ColorType.Bgra8888,
+            AlphaType.Premul,
+            image.Info.ColorSpace)
+        );
 
-            surface.Canvas.DrawImage(image, 0, 0);
-            toEncode = surface.Snapshot();
-        }
+        surface.Canvas.DrawImage(image, 0, 0);
+        toEncode = surface.Snapshot();
 
         var bitsPerPixel = toEncode.Info.BytesPerPixel == 4 ? BitsPerPixelEnum.RGBA32 : BitsPerPixelEnum.RGB24;
 
@@ -35,15 +32,5 @@ public class BmpEncoder : IImageEncoder
             toEncode.Dispose();
 
         return bitmap.GetBmpBytes(true);
-    }
-
-    private static ColorType ToValidColorType(int bytesPerPixel)
-    {
-        return bytesPerPixel switch
-        {
-            8 => ColorType.Bgra8888,
-            _ => throw new ArgumentOutOfRangeException(nameof(bytesPerPixel),
-                "Only 3 or 4 bytes per pixel are supported.")
-        };
     }
 }
