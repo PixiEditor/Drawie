3aae88929f983aad435d3b81326b0395f5668915
Krzysztof Krysiński <flubbet@gmail.com>
2025-10-31 12:59:04 +0100

    Debug SaveToDesktop

--- src/Drawie.Backend.Core/Surfaces/DrawingSurface.cs
+++ src/Drawie.Backend.Core/Surfaces/DrawingSurface.cs
@@ -104,5 +104,14 @@ namespace Drawie.Backend.Core.Surfaces
         {
             DrawingBackendApi.Current.SurfaceImplementation.Unmanage(surface);
         }
+
+#if DEBUG
+        public void SaveToDesktop()
+        {
+            using Surface surf = Surface.ForDisplay(DeviceClipBounds.Size);
+            surf.DrawingSurface.Canvas.DrawSurface(this, 0, 0);
+            surf.SaveToDesktop();
+        }
+#endif
     }
 }
